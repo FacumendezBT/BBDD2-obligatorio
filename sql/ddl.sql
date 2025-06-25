a5fe318480594f21e2086e26831ff5a9788d8e71
Facundo Méndez Bentos <facumendezbt@gmail.com>
2025-06-25 10:26:14 -0300

    DDL up to date

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,199 +1,216 @@
 CREATE DATABASE ObligatorioBD2;
-
 USE ObligatorioBD2;
 
-
-CREATE TABLE `Departamento` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `nombre` VARCHAR(100)
+CREATE TABLE Departamento (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE `Eleccion` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `fecha_hora_inicio` DATETIME,
-  `fecha_hora_fin` DATETIME,
-  `nombre` VARCHAR(255),
-  `tipo` ENUM('Presidencial', 'Ballotage', 'Referéndum', 'Plebiscito')
+CREATE TABLE Eleccion (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  fecha_hora_inicio DATETIME NOT NULL,
+  fecha_hora_fin DATETIME NOT NULL,
+  nombre VARCHAR(255) NOT NULL,
+  tipo ENUM('Presidencial', 'Ballotage', 'Referéndum', 'Plebiscito') NOT NULL
 );
 
-CREATE TABLE `Establecimiento` (
-  `dirección` VARCHAR(255) PRIMARY KEY,
-  `barrio` VARCHAR(100),
-  `pueblo` VARCHAR(100),
-  `ciudad_paraje` VARCHAR(100),
-  `zona` VARCHAR(100),
-  `tipo` ENUM('Liceo', 'Universidad', 'Escuela'),
-  `id_departamento` INT,
-  FOREIGN KEY (`id_departamento`) REFERENCES `Departamento`(`id`)
+CREATE TABLE Establecimiento (
+  direccion VARCHAR(255) PRIMARY KEY,
+  barrio VARCHAR(100),
+  pueblo VARCHAR(100),
+  ciudad_paraje VARCHAR(100),
+  zona VARCHAR(100),
+  tipo ENUM('Liceo', 'Universidad', 'Escuela') NOT NULL,
+  fk_departamento_id INT NOT NULL,
+  FOREIGN KEY (fk_departamento_id) REFERENCES Departamento(id)
 );
 
-CREATE TABLE `Circuito` (
-  `direccion_establecimiento` VARCHAR(255),
-  `nro` VARCHAR(20),
-  `es_accesible` BOOLEAN,
-  PRIMARY KEY (`direccion_establecimiento`, `nro`),
-  FOREIGN KEY (`direccion_establecimiento`) REFERENCES `Establecimiento`(`dirección`)
+CREATE TABLE Circuito (
+  fk_establecimiento_direccion VARCHAR(255),
+  nro VARCHAR(20),
+  es_accesible BOOLEAN,
+  PRIMARY KEY (fk_establecimiento_direccion, nro),
+  FOREIGN KEY (fk_establecimiento_direccion) REFERENCES Establecimiento(direccion) ON DELETE CASCADE
 );
 
-CREATE TABLE `Ciudadano` (
-  `nro_credencial` VARCHAR(50) PRIMARY KEY,
-  `cédula_identidad` VARCHAR(20) UNIQUE,
-  `fecha_nacimiento` DATE,
-  `nombre_completo` VARCHAR(255),
-  `direccion_circuito` VARCHAR(255),
-  `nro_circuito` VARCHAR(20),
-  `password_hash` VARCHAR(255),
-  FOREIGN KEY (`direccion_circuito`, `nro_circuito`) REFERENCES `Circuito`(`direccion_establecimiento`, `nro`)
+CREATE TABLE Ciudadano (
+  nro_credencial VARCHAR(50) PRIMARY KEY,
+  cedula_identidad VARCHAR(20) NOT NULL UNIQUE,
+  fecha_nacimiento DATE NOT NULL,
+  nombre_completo VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `Policia` (
-  `nro_credencial_ciudadano` VARCHAR(50) PRIMARY KEY,
-  `nro_comisaria` VARCHAR(50),
-  FOREIGN KEY (`nro_credencial_ciudadano`) REFERENCES `Ciudadano`(`nro_credencial`)
+CREATE TABLE UsuarioSistema (
+  fk_ciudadano_nro_credencial VARCHAR(50) PRIMARY KEY,
+  contraseña VARCHAR(255) NOT NULL,
+  tipo VARCHAR(100),
+  FOREIGN KEY (fk_ciudadano_nro_credencial) REFERENCES Ciudadano(nro_credencial) ON DELETE CASCADE
 );
 
-CREATE TABLE `Policia_Establecimiento` (
-  `nro_credencial_policia` VARCHAR(50),
-  `direccion_establecimiento` VARCHAR(255),
-  `fecha` DATE,
-  PRIMARY KEY (`nro_credencial_policia`, `direccion_establecimiento`, `fecha`),
-  FOREIGN KEY (`nro_credencial_policia`) REFERENCES `Policia`(`nro_credencial_ciudadano`),
-  FOREIGN KEY (`direccion_establecimiento`) REFERENCES `Establecimiento`(`dirección`)
+CREATE TABLE IntegranteMesa (
+  fk_usuariosistema_nro_credencial VARCHAR(50) PRIMARY KEY,
+  organismo_trabaja VARCHAR(255),
+  tipo ENUM('Presidente', 'Secretario', 'Vocal') NOT NULL,
+  FOREIGN KEY (fk_usuariosistema_nro_credencial) REFERENCES UsuarioSistema(fk_ciudadano_nro_credencial) ON DELETE CASCADE
 );
 
-CREATE TABLE `EstadoVoto` (
-  `tipo` ENUM('Aprobado Presidente Mesa', 'Emitido Observado', 'Emitido', 'Anulado', 'Blanco') PRIMARY KEY,
-  `descripción` TEXT
-    -- RnE: EstadoVoto.tipo "Aprobado Presidente Mesa” o "Emitido Observado” solo disponibles para voto observado. (Hay que verlo a nivel de app)
+CREATE TABLE Policia (
+  fk_ciudadano_nro_credencial VARCHAR(50) PRIMARY KEY,
+  nro_comisaria VARCHAR(50),
+  FOREIGN KEY (fk_ciudadano_nro_credencial) REFERENCES Ciudadano(nro_credencial) ON DELETE CASCADE
 );
 
-CREATE TABLE `Voto` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `direccion_circuito` VARCHAR(255),
-  `nro_circuito` VARCHAR(20),
-  FOREIGN KEY (`direccion_circuito`, `nro_circuito`) REFERENCES `Circuito`(`direccion_establecimiento`, `nro`)
+CREATE TABLE Candidato (
+  fk_ciudadano_nro_credencial VARCHAR(50) PRIMARY KEY,
+  FOREIGN KEY (fk_ciudadano_nro_credencial) REFERENCES Ciudadano(nro_credencial) ON DELETE CASCADE
 );
 
-CREATE TABLE `Voto_EstadoVoto` (
-  `id_voto` INT,
-  `tipo_estado_voto` ENUM('Aprobado Presidente Mesa', 'Emitido Observado', 'Emitido', 'Anulado', 'Blanco'),
-  `fecha_hora` DATETIME,
-  PRIMARY KEY (`id_voto`, `tipo_estado_voto`, `fecha_hora`),
-  FOREIGN KEY (`id_voto`) REFERENCES `Voto`(`id`),
-  FOREIGN KEY (`tipo_estado_voto`) REFERENCES `EstadoVoto`(`tipo`)
+CREATE TABLE ParticipanteLista (
+  fk_ciudadano_nro_credencial VARCHAR(50) PRIMARY KEY,
+  organo_candidato VARCHAR(100),
+  FOREIGN KEY (fk_ciudadano_nro_credencial) REFERENCES Ciudadano(nro_credencial) ON DELETE CASCADE
 );
 
-CREATE TABLE `IntegranteMesa` (
-  `nro_credencial_ciudadano` VARCHAR(50) PRIMARY KEY,
-  `organismo_trabaja` VARCHAR(255),
-  `tipo` ENUM('Presidente', 'Secretario', 'Vocal'),
-  FOREIGN KEY (`nro_credencial_ciudadano`) REFERENCES `Ciudadano`(`nro_credencial`)
+CREATE TABLE EstadoCircuito (
+  tipo ENUM('Abierto', 'Cerrado', 'Contado') PRIMARY KEY,
+  descripcion TEXT
 );
 
-CREATE TABLE `FormulaMesa` (
-  `id_eleccion` INT,
-  `nro_mesa` VARCHAR(20),
-  `es_accesible` BOOLEAN,
-  `id_presidente_mesa` VARCHAR(50),
-  `id_secretario_mesa` VARCHAR(50),
-  `id_vocal_mesa` VARCHAR(50),
-  `direccion_circuito` VARCHAR(255),
-  `nro_circuito` VARCHAR(20),
-  PRIMARY KEY (`id_eleccion`, `nro_mesa`),
-  FOREIGN KEY (`id_eleccion`) REFERENCES `Eleccion`(`id`),
-  FOREIGN KEY (`id_presidente_mesa`) REFERENCES `IntegranteMesa`(`nro_credencial_ciudadano`),
-  FOREIGN KEY (`id_secretario_mesa`) REFERENCES `IntegranteMesa`(`nro_credencial_ciudadano`),
-  FOREIGN KEY (`id_vocal_mesa`) REFERENCES `IntegranteMesa`(`nro_credencial_ciudadano`),
-  FOREIGN KEY (`direccion_circuito`, `nro_circuito`) REFERENCES `Circuito`(`direccion_establecimiento`, `nro`)
+CREATE TABLE FormulaMesa (
+  fk_eleccion_id INT,
+  nro_mesa VARCHAR(20),
+  es_accesible BOOLEAN,
+  estado_actual VARCHAR(100),
+  hash_integridad VARCHAR(255),
+  fk_presidente_credencial VARCHAR(50),
+  fk_secretario_credencial VARCHAR(50),
+  fk_vocal_credencial VARCHAR(50),
+  fk_circuito_establecimiento_direccion VARCHAR(255) NOT NULL,
+  fk_circuito_nro VARCHAR(20) NOT NULL,
+  PRIMARY KEY (fk_eleccion_id, nro_mesa),
+  FOREIGN KEY (fk_eleccion_id) REFERENCES Eleccion(id),
+  FOREIGN KEY (fk_presidente_credencial) REFERENCES IntegranteMesa(fk_usuariosistema_nro_credencial),
+  FOREIGN KEY (fk_secretario_credencial) REFERENCES IntegranteMesa(fk_usuariosistema_nro_credencial),
+  FOREIGN KEY (fk_vocal_credencial) REFERENCES IntegranteMesa(fk_usuariosistema_nro_credencial),
+  FOREIGN KEY (fk_circuito_establecimiento_direccion, fk_circuito_nro) REFERENCES Circuito(fk_establecimiento_direccion, nro)
 );
 
-CREATE TABLE `EstadoCircuito` (
-  `tipo` VARCHAR(50) PRIMARY KEY,
-  `descripción` TEXT,
-  CONSTRAINT `CK_EstadoCircuito_tipo` CHECK (`tipo` IN ('Abierto', 'Cerrado', 'Contado'))
+CREATE TABLE FormulaMesa_EstadoCircuito (
+  fk_formulam_eleccion_id INT,
+  fk_formulam_nro_mesa VARCHAR(20),
+  fk_estadocircuito_tipo ENUM('Abierto', 'Cerrado', 'Contado'),
+  fecha_hora DATETIME,
+  PRIMARY KEY (fk_formulam_eleccion_id, fk_formulam_nro_mesa, fecha_hora, fk_estadocircuito_tipo),
+  FOREIGN KEY (fk_formulam_eleccion_id, fk_formulam_nro_mesa) REFERENCES FormulaMesa(fk_eleccion_id, nro_mesa) ON DELETE CASCADE,
+  FOREIGN KEY (fk_estadocircuito_tipo) REFERENCES EstadoCircuito(tipo)
 );
 
-CREATE TABLE `FormulaMesa_EstadoCircuito` (
-  `id_eleccion` INT,
-  `nro_mesa` VARCHAR(20),
-  `fecha_hora` DATETIME,
-  `tipo_estado_circuito` VARCHAR(50),
-  PRIMARY KEY (`id_eleccion`, `nro_mesa`, `tipo_estado_circuito`, `fecha_hora`),
-  FOREIGN KEY (`id_eleccion`, `nro_mesa`) REFERENCES `FormulaMesa`(`id_eleccion`, `nro_mesa`),
-  FOREIGN KEY (`tipo_estado_circuito`) REFERENCES `EstadoCircuito`(`tipo`)
+CREATE TABLE Ciudadano_Circuito_Eleccion (
+  fk_ciudadano_nro_credencial VARCHAR(50),
+  fk_eleccion_id INT,
+  fk_circuito_establecimiento_direccion VARCHAR(255) NOT NULL,
+  fk_circuito_nro VARCHAR(20) NOT NULL,
+  PRIMARY KEY (fk_ciudadano_nro_credencial, fk_eleccion_id),
+  FOREIGN KEY (fk_ciudadano_nro_credencial) REFERENCES Ciudadano(nro_credencial),
+  FOREIGN KEY (fk_eleccion_id) REFERENCES Eleccion(id),
+  FOREIGN KEY (fk_circuito_establecimiento_direccion, fk_circuito_nro) REFERENCES Circuito(fk_establecimiento_direccion, nro)
 );
 
-CREATE TABLE `Vota` (
-  `id_eleccion` INT,
-  `nro_mesa` VARCHAR(20),
-  `nro_credencial_ciudadano` VARCHAR(50),
-  `fecha_hora` DATETIME,
-  PRIMARY KEY (`id_eleccion`, `nro_mesa`, `nro_credencial_ciudadano`),
-  FOREIGN KEY (`id_eleccion`, `nro_mesa`) REFERENCES `FormulaMesa`(`id_eleccion`, `nro_mesa`),
-  FOREIGN KEY (`nro_credencial_ciudadano`) REFERENCES `Ciudadano`(`nro_credencial`)
+CREATE TABLE Vota (
+  fk_formulam_eleccion_id INT,
+  fk_formulam_nro_mesa VARCHAR(20),
+  fk_ciudadano_nro_credencial VARCHAR(50),
+  id_generado VARCHAR(256) UNIQUE,
+  fecha_hora DATETIME NOT NULL,
+  PRIMARY KEY (fk_formulam_eleccion_id, fk_formulam_nro_mesa, fk_ciudadano_nro_credencial),
+  FOREIGN KEY (fk_formulam_eleccion_id, fk_formulam_nro_mesa) REFERENCES FormulaMesa(fk_eleccion_id, nro_mesa),
+  FOREIGN KEY (fk_ciudadano_nro_credencial) REFERENCES Ciudadano(nro_credencial)
 );
 
-CREATE TABLE `Papeleta` (
-  `id` INT PRIMARY KEY,
-  `descripción` TEXT,
-  `fecha_hora_fin` DATETIME,
-  `nombre` VARCHAR(255),
-  `tipo` VARCHAR(100),
-  `id_eleccion` INT,
-  FOREIGN KEY (`id_eleccion`) REFERENCES `Eleccion`(`id`)
+CREATE TABLE Voto (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  hora_emision DATETIME NOT NULL,
+  hora_contado DATETIME,
+  hash_integridad VARCHAR(255),
+  estado_actual ENUM('Aprobado Presidente Mesa', 'Emitido Observado', 'Emitido', 'Contado') NOT NULL,
+  tipo ENUM('Normal', 'Observado', 'Anulado', 'Observado Anulado', 'Blanco') NOT NULL,
+  fk_circuito_establecimiento_direccion VARCHAR(255) NOT NULL,
+  fk_circuito_nro VARCHAR(20) NOT NULL,
+  FOREIGN KEY (fk_circuito_establecimiento_direccion, fk_circuito_nro) REFERENCES Circuito(fk_establecimiento_direccion, nro),
+  CONSTRAINT CK_Voto_EstadoObservado CHECK (tipo != 'Observado' AND estado_actual NOT IN ('Emitido Observado', 'Aprobado Presidente Mesa'))
 );
 
-CREATE TABLE `Voto_Papeleta` (
-  `id_voto` INT,
-  `id_papeleta` INT,
-  PRIMARY KEY (`id_voto`, `id_papeleta`),
-  FOREIGN KEY (`id_voto`) REFERENCES `Voto`(`id`),
-  FOREIGN KEY (`id_papeleta`) REFERENCES `Papeleta`(`id`)
+CREATE TABLE Observado (
+  fk_voto_id INT PRIMARY KEY,
+  hora_aprobado DATETIME,
+  fk_integrantemesa_nro_credencial VARCHAR(50),
+  FOREIGN KEY (fk_voto_id) REFERENCES Voto(id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_integrantemesa_nro_credencial) REFERENCES IntegranteMesa(fk_usuariosistema_nro_credencial)
 );
 
-CREATE TABLE `PapeletaComun` (
-  `id_papeleta` INT PRIMARY KEY,
-  `es_si` BOOLEAN,
-  `color` VARCHAR(50),
-  FOREIGN KEY (`id_papeleta`) REFERENCES `Papeleta`(`id`)
+CREATE TABLE Papeleta (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  descripcion TEXT,
+  fk_eleccion_id INT NOT NULL,
+  FOREIGN KEY (fk_eleccion_id) REFERENCES Eleccion(id)
 );
 
-CREATE TABLE `Partido` (
-  `id` INT PRIMARY KEY,
-  `nombre` VARCHAR(255) UNIQUE,
-  `dirección_sede` VARCHAR(255),
-  `nombre_presidente` VARCHAR(255),
-  `nombre_vicepresidente` VARCHAR(255)
+CREATE TABLE Voto_Papeleta (
+  fk_voto_id INT,
+  fk_papeleta_id INT,
+  PRIMARY KEY (fk_voto_id, fk_papeleta_id),
+  FOREIGN KEY (fk_voto_id) REFERENCES Voto(id),
+  FOREIGN KEY (fk_papeleta_id) REFERENCES Papeleta(id)
 );
 
-CREATE TABLE `Candidato` (
-  `nro_credencial_ciudadano` VARCHAR(50) PRIMARY KEY,
-  FOREIGN KEY (`nro_credencial_ciudadano`) REFERENCES `Ciudadano`(`nro_credencial`)
+CREATE TABLE PapeletaComun (
+  fk_papeleta_id INT PRIMARY KEY,
+  es_si BOOLEAN, -- Representa si la papeleta es por "Sí" (true) o "No" (false).
+  color VARCHAR(50),
+  FOREIGN KEY (fk_papeleta_id) REFERENCES Papeleta(id) ON DELETE CASCADE
 );
 
-CREATE TABLE `ParticipanteLista` (
-  `nro_credencial_ciudadano` VARCHAR(50) PRIMARY KEY,
-  `organo_candidato` VARCHAR(100),
-  FOREIGN KEY (`nro_credencial_ciudadano`) REFERENCES `Ciudadano`(`nro_credencial`)
+CREATE TABLE Partido (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(255) NOT NULL UNIQUE,
+  direccion_sede VARCHAR(255),
+  nombre_presidente VARCHAR(255),
+  nombre_vicepresidente VARCHAR(255)
 );
 
-CREATE TABLE `Lista` (
-  `id_papeleta` INT,
-  `nro_lista` VARCHAR(20),
-  `nro_credencial_candidato` VARCHAR(50), -- "Cabeza de lista"
-  `id_partido` INT,
-  PRIMARY KEY (`id_papeleta`, `nro_lista`),
-  FOREIGN KEY (`id_papeleta`) REFERENCES `Papeleta`(`id`),
-  FOREIGN KEY (`nro_credencial_candidato`) REFERENCES `Candidato`(`nro_credencial_ciudadano`),
-  FOREIGN KEY (`id_partido`) REFERENCES `Partido`(`id`)
+CREATE TABLE Lista (
+  fk_partido_id INT,
+  fk_papeleta_id INT,
+  nro_lista VARCHAR(20) NOT NULL,
+  fk_departamento_id INT, -- Puede ser NULL si la lista es nacional.
+  fk_candidato_credencial VARCHAR(50), -- "Cabeza de lista".
+  PRIMARY KEY (fk_partido_id, fk_papeleta_id),
+  UNIQUE (fk_papeleta_id),
+  FOREIGN KEY (fk_partido_id) REFERENCES Partido(id),
+  FOREIGN KEY (fk_papeleta_id) REFERENCES Papeleta(id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_departamento_id) REFERENCES Departamento(id),
+  FOREIGN KEY (fk_candidato_credencial) REFERENCES Candidato(fk_ciudadano_nro_credencial)
 );
 
-CREATE TABLE `Lista_ParticipanteLista` (
-  `id_papeleta` INT,
-  `nro_lista` VARCHAR(20),
-  `nro_credencial_participante` VARCHAR(50),
-  `orden` INT,
-  PRIMARY KEY (`id_papeleta`, `nro_lista`, `nro_credencial_participante`),
-  FOREIGN KEY (`id_papeleta`, `nro_lista`) REFERENCES `Lista`(`id_papeleta`, `nro_lista`),
-  FOREIGN KEY (`nro_credencial_participante`) REFERENCES `ParticipanteLista`(`nro_credencial_ciudadano`)
+
+CREATE TABLE Policia_Establecimiento_Eleccion (
+  fk_policia_nro_credencial VARCHAR(50),
+  fk_establecimiento_direccion VARCHAR(255),
+  fk_eleccion_id INT,
+  PRIMARY KEY (fk_policia_nro_credencial, fk_establecimiento_direccion, fk_eleccion_id),
+  FOREIGN KEY (fk_policia_nro_credencial) REFERENCES Policia(fk_ciudadano_nro_credencial),
+  FOREIGN KEY (fk_establecimiento_direccion) REFERENCES Establecimiento(direccion),
+  FOREIGN KEY (fk_eleccion_id) REFERENCES Eleccion(id)
+);
+
+
+
+CREATE TABLE Lista_ParticipanteLista (
+  fk_participantelista_nro_credencial VARCHAR(50),
+  fk_lista_papeleta_id INT,
+  fk_lista_partido_id INT,
+  orden INT NOT NULL,
+  PRIMARY KEY (fk_participantelista_nro_credencial, fk_lista_papeleta_id, fk_lista_partido_id),
+  FOREIGN KEY (fk_lista_partido_id, fk_lista_papeleta_id) REFERENCES Lista(fk_partido_id, fk_papeleta_id) ON DELETE CASCADE,
+  FOREIGN KEY (fk_participantelista_nro_credencial) REFERENCES ParticipanteLista(fk_ciudadano_nro_credencial)
 );
